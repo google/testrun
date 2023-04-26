e05c383fe65b5468d58bb6ae4b8747319c9635c8
J Boddey <boddey@google.com>
2023-04-26 12:13:34 +0100

    Discover devices on the network (#5)

--- framework/testrun.py
+++ framework/testrun.py
@@ -12,108 +12,169 @@ import sys
 import json
 import signal
 import logger
+from device import Device
 
 # Locate parent directory
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
 
 LOGGER = logger.get_logger('test_run')
-CONFIG_FILE = "conf/system.json"
-EXAMPLE_CONFIG_FILE = "conf/system.json.example"
+CONFIG_FILE = 'conf/system.json'
+EXAMPLE_CONFIG_FILE = 'conf/system.json.example'
 RUNTIME = 300
 
-class TestRun: # pylint: disable=too-few-public-methods
-    """Test Run controller.
+DEVICES_DIR = 'local/devices'
+DEVICE_CONFIG = 'device_config.json'
+DEVICE_MAKE = 'make'
+DEVICE_MODEL = 'model'
+DEVICE_MAC_ADDR = 'mac_addr'
 
-    Creates an instance of the network orchestrator, test
-    orchestrator and user interface.
-    """
 
-    def __init__(self,local_net=True):
+class TestRun:  # pylint: disable=too-few-public-methods
+  """Test Run controller.
 
-        # Catch any exit signals
-        self._register_exits()
+  Creates an instance of the network orchestrator, test
+  orchestrator and user interface.
+  """
 
-        # Import the correct net orchestrator
-        self.import_orchestrators(local_net)
+  def __init__(self, local_net=True):
+    self._devices = []
 
-        self._net_orc = net_orc.NetworkOrchestrator()
-        self._test_orc = test_orc.TestOrchestrator()
+    # Catch any exit signals
+    self._register_exits()
 
-    def import_orchestrators(self,local_net=True):
-        if local_net:
-            # Add local net_orc to Python path
-            net_orc_dir = os.path.join(parent_dir, 'net_orc', 'python', 'src')
-        else:
-            # Resolve the path to the test-run parent folder
-            root_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
-            # Add manually cloned network orchestrator from parent folder
-            net_orc_dir = os.path.join(root_dir, 'network-orchestrator', 'python', 'src')
-        # Add net_orc to Python path
-        sys.path.append(net_orc_dir)
-        # Import the network orchestrator
-        global net_orc
-        import network_orchestrator as net_orc # pylint: disable=wrong-import-position,import-outside-toplevel
+    # Import the correct net orchestrator
+    self.import_dependencies(local_net)
 
-        # Add test_orc to Python path
-        test_orc_dir = os.path.join(parent_dir, 'test_orc', 'python', 'src')
-        sys.path.append(test_orc_dir)
-        global test_orc
-        import test_orchestrator as test_orc # pylint: disable=wrong-import-position,import-outside-toplevel
+    self._net_orc = net_orc.NetworkOrchestrator()
+    self._test_orc = test_orc.TestOrchestrator()
 
-    def _register_exits(self):
-        signal.signal(signal.SIGINT, self._exit_handler)
-        signal.signal(signal.SIGTERM, self._exit_handler)
-        signal.signal(signal.SIGABRT, self._exit_handler)
-        signal.signal(signal.SIGQUIT, self._exit_handler)
+  def start(self):
 
-    def _exit_handler(self, signum, arg): # pylint: disable=unused-argument
-        LOGGER.debug("Exit signal received: " + str(signum))
-        if signum in (2, signal.SIGTERM):
-            LOGGER.info("Exit signal received.")
-            self.stop_network()
+    self._load_devices()
 
-    def load_config(self):
-        """Loads all settings from the config file into memory."""
-        if not os.path.isfile(CONFIG_FILE):
-            LOGGER.error("Configuration file is not present at " + CONFIG_FILE)
-            LOGGER.info("An example is present in " + EXAMPLE_CONFIG_FILE)
-            sys.exit(1)
+    self.start_network()
 
-        with open(CONFIG_FILE, 'r', encoding='UTF-8') as config_file_open:
-            config_json = json.load(config_file_open)
-            self._net_orc.import_config(config_json)
-            self._test_orc.import_config(config_json)
+    # Register callbacks
+    self._net_orc.listener.register_callback(
+        self._device_discovered,
+        [NetworkEvent.DEVICE_DISCOVERED])
 
-    def start_network(self):
-        """Starts the network orchestrator and network services."""
+  def import_dependencies(self, local_net=True):
+    """Imports both net and test orchestrators from relevant directories."""
+    if local_net:
+      # Add local net_orc to Python path
+      net_orc_dir = os.path.join(
+          parent_dir, 'net_orc', 'python', 'src')
+    else:
+      # Resolve the path to the test-run parent folder
+      root_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
+      # Add manually cloned network orchestrator from parent folder
+      net_orc_dir = os.path.join(
+          root_dir, 'network-orchestrator', 'python', 'src')
+    # Add net_orc to Python path
+    sys.path.append(net_orc_dir)
+    # Import the network orchestrator
+    global net_orc
+    import network_orchestrator as net_orc  # pylint: disable=wrong-import-position,import-outside-toplevel
 
-        # Load and build any unbuilt network containers
-        self._net_orc.load_network_modules()
-        self._net_orc.build_network_modules()
+    # Add test_orc to Python path
+    test_orc_dir = os.path.join(
+        parent_dir, 'test_orc', 'python', 'src')
+    sys.path.append(test_orc_dir)
+    global test_orc
+    import test_orchestrator as test_orc  # pylint: disable=wrong-import-position,import-outside-toplevel
 
-        self._net_orc.stop_networking_services(kill=True)
-        self._net_orc.restore_net()
+    global NetworkEvent
+    from listener import NetworkEvent  # pylint: disable=wrong-import-position,import-outside-toplevel
 
-        # Create baseline network
-        self._net_orc.create_net()
+  def _register_exits(self):
+    signal.signal(signal.SIGINT, self._exit_handler)
+    signal.signal(signal.SIGTERM, self._exit_handler)
+    signal.signal(signal.SIGABRT, self._exit_handler)
+    signal.signal(signal.SIGQUIT, self._exit_handler)
 
-        # Launch network service containers
-        self._net_orc.start_network_services()
+  def _exit_handler(self, signum, arg):  # pylint: disable=unused-argument
+    LOGGER.debug('Exit signal received: ' + str(signum))
+    if signum in (2, signal.SIGTERM):
+      LOGGER.info('Exit signal received.')
+      self.stop_network()
 
-        LOGGER.info("Network is ready.")
+  def load_config(self):
+    """Loads all settings from the config file into memory."""
+    if not os.path.isfile(CONFIG_FILE):
+      LOGGER.error(
+          'Configuration file is not present at ' + CONFIG_FILE)
+      LOGGER.info('An example is present in ' + EXAMPLE_CONFIG_FILE)
+      sys.exit(1)
 
-    def run_tests(self):
-        """Iterate through and start all test modules."""
+    with open(CONFIG_FILE, 'r', encoding='UTF-8') as config_file_open:
+      config_json = json.load(config_file_open)
+      self._net_orc.import_config(config_json)
+      self._test_orc.import_config(config_json)
 
-        self._test_orc.load_test_modules()
-        self._test_orc.build_test_modules()
+  def start_network(self):
+    """Starts the network orchestrator and network services."""
 
-        # Begin testing
-        self._test_orc.run_test_modules()
+    # Load and build any unbuilt network containers
+    self._net_orc.load_network_modules()
+    self._net_orc.build_network_modules()
 
-    def stop_network(self):
-        """Commands the net_orc to stop the network and clean up."""
-        self._net_orc.stop_networking_services(kill=True)
-        self._net_orc.restore_net()
-        sys.exit(0)
+    self._net_orc.stop_networking_services(kill=True)
+    self._net_orc.restore_net()
+
+    # Create baseline network
+    self._net_orc.create_net()
+
+    # Launch network service containers
+    self._net_orc.start_network_services()
+
+    LOGGER.info('Network is ready.')
+
+  def run_tests(self):
+    """Iterate through and start all test modules."""
+    self._test_orc.load_test_modules()
+    self._test_orc.build_test_modules()
+
+    # Begin testing
+    self._test_orc.run_test_modules()
+
+  def stop_network(self):
+    """Commands the net_orc to stop the network and clean up."""
+    self._net_orc.stop_networking_services(kill=True)
+    self._net_orc.restore_net()
+    sys.exit(0)
+
+  def _load_devices(self):
+    LOGGER.debug('Loading devices from ' + DEVICES_DIR)
+
+    for device_folder in os.listdir(DEVICES_DIR):
+      with open(os.path.join(DEVICES_DIR, device_folder, DEVICE_CONFIG),
+                encoding='utf-8') as device_config_file:
+        device_config_json = json.load(device_config_file)
+
+        device_make = device_config_json.get(DEVICE_MAKE)
+        device_model = device_config_json.get(DEVICE_MODEL)
+        mac_addr = device_config_json.get(DEVICE_MAC_ADDR)
+
+        device = Device(device_make, device_model,
+                        mac_addr=mac_addr)
+        self._devices.append(device)
+
+    LOGGER.info('Loaded ' + str(len(self._devices)) + ' devices')
+
+  def get_device(self, mac_addr):
+    """Returns a loaded device object from the device mac address."""
+    for device in self._devices:
+      if device.mac_addr == mac_addr:
+        return device
+    return None
+
+  def _device_discovered(self, mac_addr):
+    device = self.get_device(mac_addr)
+    if device is not None:
+      LOGGER.info(
+        f'Discovered {device.make} {device.model} on the network')
+    else:
+      LOGGER.info(
+        f'A new device has been discovered with mac address {device.mac_addr}')
