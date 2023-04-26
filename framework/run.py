e21a7571801d0b99ab38294162e33ca1c321b6b0
jhughesbiot <jonathan.hughes@buildingsiot.com>
2023-04-26 09:55:31 -0600

    Add config file arg Misc changes to network start procedure

--- framework/run.py
+++ framework/run.py
@@ -9,13 +9,13 @@ LOGGER = logger.get_logger('runner')
 
 class TestRunner:
 
-    def __init__(self, local_net=True):
+    def __init__(self, local_net=True,config_file=None, argv=None):
 
         LOGGER.info('Starting Test Run')
 
-        testrun = TestRun(local_net)
+        testrun = TestRun(local_net=local_net,argsv=argv)
 
-        testrun.load_config()
+        testrun.load_config(config_file=config_file)
 
         testrun.start_network()
 
@@ -30,10 +30,12 @@ def run(argv):
     parser.add_argument("-r", "--remote-net", action="store_false",
                         help='''Use the network orchestrator from the parent directory instead
                         		of the one downloaded locally from the install script.''')
+    parser.add_argument("-f","--config-file", default=None, 
+                        help="Define the configuration file for Test Run and Network Orchestrator")
 
     args, unknown = parser.parse_known_args()
 
-    TestRunner(args.remote_net)
+    TestRunner(local_net=args.remote_net,config_file=args.config_file, argv=argv)
 
 
 if __name__ == "__main__":
--- framework/testrun.py
+++ framework/testrun.py
@@ -6,7 +6,6 @@ Test Run components, such as net_orc, test_orc and test_ui.
 Run using the provided command scripts in the cmd folder.
 E.g sudo cmd/start
 """
-
 import os
 import sys
 import json
@@ -22,25 +21,27 @@ CONFIG_FILE = "conf/system.json"
 EXAMPLE_CONFIG_FILE = "conf/system.json.example"
 RUNTIME = 300
 
-class TestRun: # pylint: disable=too-few-public-methods
+
+class TestRun:  # pylint: disable=too-few-public-methods
     """Test Run controller.
 
     Creates an instance of the network orchestrator, test
     orchestrator and user interface.
     """
 
-    def __init__(self,local_net=True):
+    def __init__(self, local_net=True, argsv=None):
 
         # Catch any exit signals
         self._register_exits()
 
         # Import the correct net orchestrator
-        self.import_orchestrators(local_net)
+        self.import_dependencies(local_net)
 
         self._net_orc = net_orc.NetworkOrchestrator()
         self._test_orc = test_orc.TestOrchestrator()
+        self._net_run = net_run.NetworkRunner(argsv)
 
-    def import_orchestrators(self,local_net=True):
+    def import_dependencies(self, local_net=True):
         if local_net:
             # Add local net_orc to Python path
             net_orc_dir = os.path.join(parent_dir, 'net_orc', 'python', 'src')
@@ -48,18 +49,22 @@ class TestRun: # pylint: disable=too-few-public-methods
             # Resolve the path to the test-run parent folder
             root_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
             # Add manually cloned network orchestrator from parent folder
-            net_orc_dir = os.path.join(root_dir, 'network-orchestrator', 'python', 'src')
+            net_orc_dir = os.path.join(
+                root_dir, 'network-orchestrator', 'python', 'src')
         # Add net_orc to Python path
         sys.path.append(net_orc_dir)
         # Import the network orchestrator
         global net_orc
-        import network_orchestrator as net_orc # pylint: disable=wrong-import-position,import-outside-toplevel
+        import network_orchestrator as net_orc  # pylint: disable=wrong-import-position,import-outside-toplevel
 
         # Add test_orc to Python path
         test_orc_dir = os.path.join(parent_dir, 'test_orc', 'python', 'src')
         sys.path.append(test_orc_dir)
         global test_orc
-        import test_orchestrator as test_orc # pylint: disable=wrong-import-position,import-outside-toplevel
+        import test_orchestrator as test_orc  # pylint: disable=wrong-import-position,import-outside-toplevel
+
+        global net_run
+        import network_runner as net_run  # pylint: disable=wrong-import-position,import-outside-toplevel
 
     def _register_exits(self):
         signal.signal(signal.SIGINT, self._exit_handler)
@@ -67,41 +72,34 @@ class TestRun: # pylint: disable=too-few-public-methods
         signal.signal(signal.SIGABRT, self._exit_handler)
         signal.signal(signal.SIGQUIT, self._exit_handler)
 
-    def _exit_handler(self, signum, arg): # pylint: disable=unused-argument
+    def _exit_handler(self, signum, arg):  # pylint: disable=unused-argument
         LOGGER.debug("Exit signal received: " + str(signum))
         if signum in (2, signal.SIGTERM):
             LOGGER.info("Exit signal received.")
             self.stop_network()
 
-    def load_config(self):
+    def load_config(self, config_file=None):
+        if config_file is None:
+            # If not defined, use relative pathing to local file
+            self._config_file = os.path.join(parent_dir, CONFIG_FILE)
+        else:
+            # If defined, use as provided
+            self._config_file = config_file
+
         """Loads all settings from the config file into memory."""
-        if not os.path.isfile(CONFIG_FILE):
-            LOGGER.error("Configuration file is not present at " + CONFIG_FILE)
+        if not os.path.isfile(self._config_file):
+            LOGGER.error("Configuration file is not present at " + self._config_file)
             LOGGER.info("An example is present in " + EXAMPLE_CONFIG_FILE)
             sys.exit(1)
 
-        with open(CONFIG_FILE, 'r', encoding='UTF-8') as config_file_open:
-            config_json = json.load(config_file_open)
-            self._net_orc.import_config(config_json)
+        LOGGER.info("Loading Config File: " + os.path.abspath(self._config_file)) 
+
+        with open(self._config_file, encoding='UTF-8') as config_json_file:
+            config_json = json.load(config_json_file)
             self._test_orc.import_config(config_json)
 
     def start_network(self):
-        """Starts the network orchestrator and network services."""
-
-        # Load and build any unbuilt network containers
-        self._net_orc.load_network_modules()
-        self._net_orc.build_network_modules()
-
-        self._net_orc.stop_networking_services(kill=True)
-        self._net_orc.restore_net()
-
-        # Create baseline network
-        self._net_orc.create_net()
-
-        # Launch network service containers
-        self._net_orc.start_network_services()
-
-        LOGGER.info("Network is ready.")
+        self._net_run.run()
 
     def run_tests(self):
         """Iterate through and start all test modules."""
@@ -113,7 +111,4 @@ class TestRun: # pylint: disable=too-few-public-methods
         self._test_orc.run_test_modules()
 
     def stop_network(self):
-        """Commands the net_orc to stop the network and clean up."""
-        self._net_orc.stop_networking_services(kill=True)
-        self._net_orc.restore_net()
-        sys.exit(0)
+        self._net_run._stop()
