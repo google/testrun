4f2c43532d0ef2e49af24138e8ea50305d0143bc
jhughesbiot <jonathan.hughes@buildingsiot.com>
2023-05-05 16:24:56 -0600

    Update TCP port scanning range Update logging

--- test_orc/modules/nmap/python/src/nmap_module.py
+++ test_orc/modules/nmap/python/src/nmap_module.py
@@ -21,11 +21,10 @@ class NmapModule(TestModule):
         LOGGER.info(
             "Running security.nmap.ports test")
         if self._device_ipv4_addr is not None:
-            self._scan_tcp_results = self._scan_tcp_ports()
+            self._scan_tcp_results = self._scan_tcp_ports(config)
             self._scan_udp_results = self._scan_udp_ports(config)
-            LOGGER.info("TCP Scan Results: " + str(self._scan_tcp_results))
-            LOGGER.info("UDP Scan Results: " + str(self._scan_udp_results))
-            LOGGER.info("nmap scan test finished")
+            LOGGER.debug("TCP scan results: " + str(self._scan_tcp_results))
+            LOGGER.debug("UDP scan results: " + str(self._scan_udp_results))
             self._process_port_results(
                 tests=config)
             LOGGER.info("Unallowed Ports: " + str(self._unallowed_ports))
@@ -36,7 +35,7 @@ class NmapModule(TestModule):
 
     def _process_port_results(self, tests):
         for test in tests:
-            LOGGER.info("Checking test: " + str(test))
+            LOGGER.info("Checking results for test: " + str(test))
             self._check_scan_results(test_config=tests[test])
 
     def _check_scan_results(self, test_config):
@@ -45,6 +44,7 @@ class NmapModule(TestModule):
             port_config.update(test_config["tcp_ports"])
         elif "udp_ports" in test_config:
             port_config.update(test_config["udp_ports"])
+            
         scan_results = {}
         if self._scan_tcp_results is not None:
             scan_results.update(self._scan_tcp_results)
@@ -53,14 +53,14 @@ class NmapModule(TestModule):
         if port_config is not None:
             for port in port_config:
                 result = None
-                LOGGER.info("Checking Port: " + str(port))
-                LOGGER.debug("Port Config: " + str(port_config[port]))
+                LOGGER.info("Checking port: " + str(port))
+                LOGGER.debug("Port config: " + str(port_config[port]))
                 if port in scan_results:
                     if scan_results[port]["state"] == "open":
                         if not port_config[port]["allowed"]:
                             LOGGER.info("Unallowed port open")
-                            result = False
                             self._unallowed_ports.append(str(port))
+                            result = False
                         else:
                             LOGGER.info("Allowed port open")
                             result = True
@@ -76,9 +76,22 @@ class NmapModule(TestModule):
                 else:
                     port_config[port]["result"] = "skipped"
 
-    def _scan_tcp_ports(self):
-        LOGGER.info( "Running nmap TCP port scans")
-        nmap_results, err = util.run_command("nmap -sT -sV -Pn -v -p 1-100,5357 --allports --version-intensity 7 -T4 " + self._device_ipv4_addr)
+    def _scan_tcp_ports(self, tests):
+        max_port = 1000
+        ports = []
+        for test in tests:
+            test_config = tests[test]
+            if "tcp_ports" in test_config:
+                    for port in test_config["tcp_ports"]:
+                        if int(port) > max_port:
+                            ports.append(port)
+        ports_to_scan="1-" + str(max_port)
+        if len(ports) > 0:                     
+            ports_to_scan += "," + ','.join(ports)
+        LOGGER.info("Running nmap TCP port scan")
+        LOGGER.info("TCP ports: " + str(ports_to_scan))
+        nmap_results, err = util.run_command("nmap -sT -sV -Pn -v -p " + ports_to_scan + " --allports --version-intensity 7 -T4 " + self._device_ipv4_addr)
+        LOGGER.info("TCP port scan complete")
         return self._process_nmap_results(nmap_results=nmap_results)
 
     def _scan_udp_ports(self, tests):
@@ -90,15 +103,16 @@ class NmapModule(TestModule):
                         ports.append(port)
         if len(ports) > 0:
             port_list = ','.join(ports)
-            LOGGER.info("UDP Ports: " + str(port_list))
-            LOGGER.info( "Running nmap UDP port scans")
+            LOGGER.info("Running nmap UDP port scan")
+            LOGGER.info("UDP ports: " + str(port_list))
             nmap_results, err = util.run_command("nmap -sU -sV -p " + port_list + " " + self._device_ipv4_addr)
+            LOGGER.info("UDP port scan complete")
             return self._process_nmap_results(nmap_results=nmap_results)
         return None
 
     def _process_nmap_results(self,nmap_results):
         results = {}
-        LOGGER.info("nmap results\n" + str(nmap_results))
+        LOGGER.debug("nmap results\n" + str(nmap_results))
         if nmap_results:
             if "Service Info" in nmap_results:
                 rows = nmap_results.split("PORT")[1].split("Service Info")[0].split("\n")
@@ -107,6 +121,8 @@ class NmapModule(TestModule):
             for result in rows[1:-1]:  # Iterate skipping the header and tail rows
                 cols = result.split(" ")
                 port_result = {cols[0].split(
-                    "/")[0]: {"state": cols[1], "service": cols[2]}}
+                    "/")[0]: {"state": cols[1], "service": cols[2], "version":""}}
+                if len(cols) > 3:
+                    port_result["version"]=cols[3]
                 results.update(port_result)
         return results
