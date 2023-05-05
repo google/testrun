82175424d0cbc53678d223a6ce67b3b39966fdc5
jhughesbiot <jonathan.hughes@buildingsiot.com>
2023-05-05 15:29:53 -0600

    Update nmap commands to match existing DAQ tests Add udp scanning and tests

--- test_orc/modules/nmap/python/src/nmap_module.py
+++ test_orc/modules/nmap/python/src/nmap_module.py
@@ -22,8 +22,10 @@ class NmapModule(TestModule):
         LOGGER.info(
             "Running nmap scan test")
         if self._device_ipv4_addr is not None:
-            self._scan_tcp_results = self._scan_for_tcp_ports()
-            LOGGER.info("Port Scan Results: " + str(self._scan_tcp_results))
+            self._scan_tcp_results = self._scan_tcp_ports()
+            self._scan_udp_results = self._scan_udp_ports(config)
+            LOGGER.info("TCP Scan Results: " + str(self._scan_tcp_results))
+            LOGGER.info("UDP Scan Results: " + str(self._scan_udp_results))
             LOGGER.info("nmap scan test finished")
             self._process_port_results(
                 tests=config)
@@ -35,21 +37,31 @@ class NmapModule(TestModule):
 
     def _process_port_results(self, tests):
         for test in tests:
-            LOGGER.info("Running test: " + str(test))
+            LOGGER.info("Checking test: " + str(test))
             self._check_scan_results(test_config=tests[test])
 
     def _check_scan_results(self, test_config):
+        port_config = {}
         if "tcp_ports" in test_config:
-            tcp_port_config = test_config["tcp_ports"]
+            port_config.update(test_config["tcp_ports"])
+        elif "udp_ports" in test_config:
+            port_config.update(test_config["udp_ports"])
 
-        if tcp_port_config is not None:
-            for port in tcp_port_config:
+        LOGGER.info("Ports Config: " + str(port_config))
+        scan_results = {}
+        if self._scan_tcp_results is not None:
+            scan_results.update(self._scan_tcp_results)
+        if self._scan_udp_results is not None:
+            scan_results.update(self._scan_udp_results)
+        LOGGER.info("Scan Results: " + str(scan_results))
+        if port_config is not None:
+            for port in port_config:
                 result = None
                 LOGGER.info("Checking Port: " + str(port))
-                LOGGER.debug("Port Config: " + str(tcp_port_config[port]))
-                if port in self._scan_tcp_results:
-                    if self._scan_tcp_results[port]["state"] == "open":
-                        if not tcp_port_config[port]["allowed"]:
+                LOGGER.debug("Port Config: " + str(port_config[port]))
+                if port in scan_results:
+                    if scan_results[port]["state"] == "open":
+                        if not port_config[port]["allowed"]:
                             LOGGER.info("Unallowed port open")
                             result = False
                             self._unallowed_ports.append(str(port))
@@ -64,16 +76,38 @@ class NmapModule(TestModule):
                     result = True
 
                 if result is not None:
-                    tcp_port_config[port]["result"] = "compliant" if result else "non-compliant"
+                    port_config[port]["result"] = "compliant" if result else "non-compliant"
                 else:
-                    tcp_port_config[port]["result"] = "skipped"
+                    port_config[port]["result"] = "skipped"
 
-    def _scan_for_tcp_ports(self):
+    def _scan_tcp_ports(self):
+        LOGGER.info( "Running nmap TCP port scans")
+        nmap_results, err = util.run_command("nmap -sT -sV -Pn -v -p 1-100,5357 --allports --version-intensity 7 -T4 " + self._device_ipv4_addr)
+        return self._process_nmap_results(nmap_results=nmap_results)
+
+    def _scan_udp_ports(self, tests):
+        ports = []
+        for test in tests:
+            test_config = tests[test]
+            if "udp_ports" in test_config:
+                    for port in test_config["udp_ports"]:
+                        ports.append(port)
+        if len(ports) > 0:
+            port_list = ','.join(ports)
+            LOGGER.info("UDP Ports: " + str(port_list))
+            LOGGER.info( "Running nmap UDP port scans")
+            nmap_results, err = util.run_command("nmap -sU -sV -p " + port_list + " " + self._device_ipv4_addr)
+            return self._process_nmap_results(nmap_results=nmap_results)
+        return None
+
+    def _process_nmap_results(self,nmap_results):
         results = {}
-        text, err = util.run_command("nmap " + self._device_ipv4_addr)
-        LOGGER.info("nmap results\n" + str(text))
-        if text:
-            rows = text.split("PORT")[1].split("MAC Address")[0].split("\n")
+        LOGGER.info("nmap results\n" + str(nmap_results))
+        if nmap_results:
+            if "Service Info" in nmap_results:
+                rows = nmap_results.split("PORT")[1].split("Service Info")[0].split("\n")
+            else:
+                rows = nmap_results.split("PORT")[1].split("MAC Address")[0].split("\n")
             for result in rows[1:-1]:  # Iterate skipping the header and tail rows
                 cols = result.split(" ")
                 port_result = {cols[0].split(
