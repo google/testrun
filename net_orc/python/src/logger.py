ef22c4a38f23613ad8e3e70148fe0eaa4013e0f8
jhughesbiot <jonathan.hughes@buildingsiot.com>
2023-05-17 16:13:01 -0600

    fix logger lint issues

--- net_orc/python/src/logger.py
+++ net_orc/python/src/logger.py
@@ -4,16 +4,19 @@ import logging
 import os
 
 LOGGERS = {}
-_LOG_FORMAT = "%(asctime)s %(name)-8s %(levelname)-7s %(message)s"
+_LOG_FORMAT = '%(asctime)s %(name)-8s %(levelname)-7s %(message)s'
 _DATE_FORMAT = '%b %02d %H:%M:%S'
 _DEFAULT_LEVEL = logging.INFO
-_CONF_DIR = "conf"
-_CONF_FILE_NAME = "system.json"
+_CONF_DIR = 'conf'
+_CONF_FILE_NAME = 'system.json'
 
 # Set log level
 try:
-  system_conf_json = json.load(
-      open(os.path.join(_CONF_DIR, _CONF_FILE_NAME), encoding='UTF-8'))
+
+  with open(os.path.join(_CONF_DIR, _CONF_FILE_NAME),
+            encoding='UTF-8') as config_json_file:
+    system_conf_json = json.load(config_json_file)
+
   log_level_str = system_conf_json['log_level']
   LOG_LEVEL = logging.getLevelName(log_level_str)
 except OSError:
