079b72373f5e6a11a58253785496ec8c1aed0e66
jhughesbiot <jonathan.hughes@buildingsiot.com>
2023-05-18 15:40:47 -0600

    Update base network module linting issues

--- net_orc/network/modules/base/python/src/grpc/start_server.py
+++ net_orc/network/modules/base/python/src/grpc/start_server.py
@@ -1,34 +1,37 @@
+"""Base class for starting the gRPC server for a network module."""
 from concurrent import futures
 import grpc
 import proto.grpc_pb2_grpc as pb2_grpc
-import proto.grpc_pb2 as pb2
 from network_service import NetworkService
-import logging
-import sys
 import argparse
 
 DEFAULT_PORT = '5001'
 
-def serve(PORT):
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    pb2_grpc.add_NetworkModuleServicer_to_server(NetworkService(), server)
-    server.add_insecure_port('[::]:' + PORT)
-    server.start()
-    server.wait_for_termination()
 
-def run(argv):
-    parser = argparse.ArgumentParser(description="GRPC Server for Network Module",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-p", "--port", default=DEFAULT_PORT,
-                        help="Define the default port to run the server on.")
-
-    args = parser.parse_args()
-
-    PORT = args.port
-
-    print("gRPC server starting on port " + PORT)
-    serve(PORT)
+def serve(port):
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+  pb2_grpc.add_NetworkModuleServicer_to_server(NetworkService(), server)
+  server.add_insecure_port('[::]:' + port)
+  server.start()
+  server.wait_for_termination()
 
 
-if __name__ == "__main__":
-    run(sys.argv)
+def run():
+  parser = argparse.ArgumentParser(
+      description='GRPC Server for Network Module',
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('-p',
+                      '--port',
+                      default=DEFAULT_PORT,
+                      help='Define the default port to run the server on.')
+
+  args = parser.parse_args()
+
+  port = args.port
+
+  print('gRPC server starting on port ' + port)
+  serve(port)
+
+
+if __name__ == '__main__':
+  run()
