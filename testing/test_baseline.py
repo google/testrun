227694d600072634019ccf656326cde981d8bc2e
Jacob Boddey <boddey@google.com>
2023-05-25 11:30:38 +0100

    More formatting fixes

--- testing/test_baseline.py
+++ testing/test_baseline.py
@@ -1,7 +1,7 @@
 import json
 import pytest
 import re
-import os 
+import os
 
 NTP_SERVER = '10.10.10.5'
 DNS_SERVER = '10.10.10.4'
@@ -10,40 +10,43 @@ CI_BASELINE_OUT = '/tmp/testrun_ci.json'
 
 @pytest.fixture
 def container_data():
-    dir = os.path.dirname(os.path.abspath(__file__))
-    with open(CI_BASELINE_OUT) as f:
-       return json.load(f)
+  dir = os.path.dirname(os.path.abspath(__file__))
+  with open(CI_BASELINE_OUT, encoding='utf-8') as f:
+    return json.load(f)
 
 @pytest.fixture
 def validator_results():
-    dir = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(dir, '../', 'runtime/validation/faux-dev/result.json')) as f:
-       return json.load(f)
+  dir = os.path.dirname(os.path.abspath(__file__))
+  with open(os.path.join(dir,
+                         '../',
+                         'runtime/validation/faux-dev/result.json'),
+                         encoding='utf-8') as f:
+    return json.load(f)
 
 def test_internet_connectivity(container_data):
-    assert container_data['network']['internet'] == 200
+  assert container_data['network']['internet'] == 200
 
 def test_dhcp_ntp_option(container_data):
-    """ Check DHCP gives NTP server as option """
-    assert container_data['dhcp']['ntp-servers'] == NTP_SERVER
+  """ Check DHCP gives NTP server as option """
+  assert container_data['dhcp']['ntp-servers'] == NTP_SERVER
 
 def test_dhcp_dns_option(container_data):
-    assert container_data['dhcp']['domain-name-servers'] == DNS_SERVER
+  assert container_data['dhcp']['domain-name-servers'] == DNS_SERVER
 
 def test_assigned_ipv4_address(container_data):
-    assert int(container_data['network']['ipv4'].split('.')[-1][:-3]) > 10
+  assert int(container_data['network']['ipv4'].split('.')[-1][:-3]) > 10
 
 def test_ntp_server_reachable(container_data):
-    assert not 'no servers' in container_data['ntp_offset']
+  assert not 'no servers' in container_data['ntp_offset']
 
 def test_dns_server_reachable(container_data):
-    assert not 'no servers' in container_data['dns_response']
+  assert not 'no servers' in container_data['dns_response']
 
 def test_dns_server_resolves(container_data):
-    assert re.match(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', 
-        container_data['dns_response'])
+  assert re.match(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}',
+    container_data['dns_response'])
 
 def test_validator_results_compliant(validator_results):
-    results = [True if x['result'] == 'compliant' else False 
-        for x in validator_results['results']]
-    assert all(results)
+  results = [True if x['result'] == 'compliant' else False
+    for x in validator_results['results']]
+  assert all(results)
