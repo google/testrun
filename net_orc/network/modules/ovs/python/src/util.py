7fccfbb3e3191824ec05f7b7db104ccfc8fd69ce
jhughesbiot <jonathan.hughes@buildingsiot.com>
2023-05-26 12:02:06 -0600

    ovs module formatting

--- net_orc/network/modules/ovs/python/src/util.py
+++ net_orc/network/modules/ovs/python/src/util.py
@@ -1,21 +1,23 @@
+"""Provides basic utilities for a ovs module."""
 import subprocess
 import logger
 
+LOGGER = logger.get_logger('util')
 
 def run_command(cmd):
   success = False
-  LOGGER = logger.get_logger('util')
-  process = subprocess.Popen(cmd.split(), 
-                             stdout=subprocess.PIPE, 
+  process = subprocess.Popen(cmd.split(),
+                             stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
   stdout, stderr = process.communicate()
-  if process.returncode !=0:
-    err_msg = "%s. Code: %s" % (stderr.strip(), process.returncode)
-    LOGGER.error("Command Failed: " + cmd)
-    LOGGER.error("Error: " + err_msg)
+  if process.returncode != 0:
+    err_msg = f'{stderr.strip()}. Code: {process.returncode}'
+    LOGGER.error('Command Failed: ' + cmd)
+    LOGGER.error('Error: ' + err_msg)
   else:
-    succ_msg = "%s. Code: %s" % (stdout.strip().decode('utf-8'), process.returncode)
-    LOGGER.info("Command Success: " + cmd)
-    LOGGER.info("Success: " + succ_msg)
+    msg = stdout.strip().decode('utf-8')
+    succ_msg = f'{msg}. Code: {process.returncode}'
+    LOGGER.info('Command Success: ' + cmd)
+    LOGGER.info('Success: ' + succ_msg)
     success = True
   return success
