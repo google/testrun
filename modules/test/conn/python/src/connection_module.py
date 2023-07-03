d4e3e9102f7eafc662989fbf897be1722bc202f8
jhughesbiot <jonathan.hughes@buildingsiot.com>
2023-07-03 14:03:56 -0600

    misc cleanup

--- modules/test/conn/python/src/connection_module.py
+++ modules/test/conn/python/src/connection_module.py
@@ -36,29 +36,31 @@ class ConnectionModule(TestModule):
     LOGGER = self._get_logger()
     self.dhcp1_client = DHCPClient1()
     
-    response = self.dhcp1_client.add_reserved_lease('test','00:11:22:33:44:55','10.10.10.21')
-    print("AddLeaseResp: " + str(response))
+    # ToDo: Move this into some level of testing, leave for 
+    # reference until tests are implemented with these calls
+    # response = self.dhcp1_client.add_reserved_lease('test','00:11:22:33:44:55','10.10.10.21')
+    # print("AddLeaseResp: " + str(response))
 
-    response = self.dhcp1_client.delete_reserved_lease('00:11:22:33:44:55')
-    print("DelLeaseResp: " + str(response))
+    # response = self.dhcp1_client.delete_reserved_lease('00:11:22:33:44:55')
+    # print("DelLeaseResp: " + str(response))
 
-    response = self.dhcp1_client.disable_failover()
-    print("FailoverDisabled: " + str(response))
+    # response = self.dhcp1_client.disable_failover()
+    # print("FailoverDisabled: " + str(response))
 
-    response = self.dhcp1_client.enable_failover()
-    print("FailoverEnabled: " + str(response))
+    # response = self.dhcp1_client.enable_failover()
+    # print("FailoverEnabled: " + str(response))
 
-    response = self.dhcp1_client.get_dhcp_range()
-    print("DHCP Range: " + str(response))
+    # response = self.dhcp1_client.get_dhcp_range()
+    # print("DHCP Range: " + str(response))
 
-    response = self.dhcp1_client.get_lease(self._device_mac)
-    print("Lease: " + str(response))
+    # response = self.dhcp1_client.get_lease(self._device_mac)
+    # print("Lease: " + str(response))
 
-    response = self.dhcp1_client.get_status()
-    print("Status: " + str(response))
+    # response = self.dhcp1_client.get_status()
+    # print("Status: " + str(response))
 
-    response = self.dhcp1_client.set_dhcp_range('10.10.10.20','10.10.10.30')
-    print("Set Range: " + str(response))
+    # response = self.dhcp1_client.set_dhcp_range('10.10.10.20','10.10.10.30')
+    # print("Set Range: " + str(response))
 
 
   def _connection_mac_address(self):
