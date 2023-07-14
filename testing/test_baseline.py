24366725c907948a2d7f2e0356a87279f18042e2
Noureddine <noureddine@saidi.uk>
2023-07-14 09:51:17 +0000

    Initial CI testing for tests (#72)

--- testing/test_baseline.py
+++ testing/test_baseline.py
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+""" Test assertions for CI network baseline test """
+# Temporarily disabled because using Pytest fixtures
+# TODO refactor fixtures to not trigger error
+# pylint: disable=redefined-outer-name
+
 import json
 import pytest
 import re
@@ -24,14 +29,13 @@ CI_BASELINE_OUT = '/tmp/testrun_ci.json'
 
 @pytest.fixture
 def container_data():
-  dir = os.path.dirname(os.path.abspath(__file__))
   with open(CI_BASELINE_OUT, encoding='utf-8') as f:
     return json.load(f)
 
 @pytest.fixture
 def validator_results():
-  dir = os.path.dirname(os.path.abspath(__file__))
-  with open(os.path.join(dir,
+  basedir = os.path.dirname(os.path.abspath(__file__))
+  with open(os.path.join(basedir,
                          '../',
                          'runtime/validation/faux-dev/result.json'),
                          encoding='utf-8') as f:
@@ -63,6 +67,5 @@ def test_dns_server_resolves(container_data):
 
 @pytest.mark.skip(reason='requires internet')
 def test_validator_results_compliant(validator_results):
-  results = [True if x['result'] == 'compliant' else False
-    for x in validator_results['results']]
+  results = [x['result'] == 'compliant' for x in validator_results['results']]
   assert all(results)
