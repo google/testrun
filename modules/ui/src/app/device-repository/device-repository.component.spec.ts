b7fc84d7acea7a05e4193209016934d4854dd8f3
Jacob Boddey <boddey@google.com>
2023-09-22 14:51:42 +0100

    Update UI

--- modules/ui/src/app/device-repository/device-repository.component.spec.ts
+++ modules/ui/src/app/device-repository/device-repository.component.spec.ts
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 import {of} from 'rxjs';
 import {Device} from '../model/device';
@@ -9,8 +24,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DeviceFormComponent} from './device-form/device-form.component';
 import {MatDialogRef} from '@angular/material/dialog';
 import {BehaviorSubject} from 'rxjs/internal/BehaviorSubject';
-import SpyObj = jasmine.SpyObj;
 import {device} from '../mocks/device.mock';
+import SpyObj = jasmine.SpyObj;
 
 describe('DeviceRepositoryComponent', () => {
   let service: TestRunService;
