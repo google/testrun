129870bdb52901cf41949cd82982634fe2049e56
J Boddey <boddey@google.com>
2023-09-26 14:10:31 +0000

    Update UI (#125)
    * Update UI
    
    * Update UI

--- modules/ui/src/app/app-routing.module.ts
+++ modules/ui/src/app/app-routing.module.ts
@@ -1,6 +1,22 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {allowToRunTestGuard} from './guards/allow-to-run-test.guard';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const routes: Routes = [
   {
@@ -26,7 +42,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class AppRoutingModule {
 }
--- modules/ui/src/app/model/testrun-status.ts
+++ modules/ui/src/app/model/testrun-status.ts
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import {Device} from './device';
 
 export interface TestrunStatus {
@@ -38,16 +53,19 @@ export enum StatusOfTestrun {
 }
 
 export enum StatusOfTestResult {
-  Compliant = 'Compliant',
+  Compliant = 'Compliant', // device supports feature
   SmartReady = 'Smart Ready',
-  NonCompliant = 'Non-Compliant',
+  NonCompliant = 'Non-Compliant', // device does not support feature but feature is required
   Skipped = 'Skipped',
-  NotStarted = 'Not Started'
+  NotStarted = 'Not Started',
+  Error = 'Error', // test failed to run
+  Info = 'Informational' // nice to know information, not necessarily compliant/non-compliant
 }
 
 export interface StatusResultClassName {
   green: boolean,
   red: boolean,
+  blue: boolean,
   grey: boolean
 }
 
