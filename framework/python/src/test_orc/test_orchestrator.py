313a90b27fd595d75413624bef437cf13ccf17f2
Jacob Boddey <boddey@google.com>
2023-09-26 08:36:27 +0100

    Move test count

--- framework/python/src/test_orc/test_orchestrator.py
+++ framework/python/src/test_orc/test_orchestrator.py
@@ -78,8 +78,22 @@ class TestOrchestrator:
     self._test_in_progress = True
     LOGGER.info(
         f"Running test modules on device with mac addr {device.mac_addr}")
+
+    test_modules = []
     for module in self._test_modules:
+
+      if module is None or not module.enable_container or not module.enabled:
+        continue
+
+      if not self._is_module_enabled(module, device):
+        continue
+
+      test_modules.append(module)
+      self.get_session().add_total_tests(len(module.tests))
+
+    for module in test_modules:
       self._run_test_module(module)
+
     LOGGER.info("All tests complete")
 
     self._session.stop()
@@ -243,12 +257,6 @@ class TestOrchestrator:
 
     device = self._session.get_target_device()
 
-    if module is None or not module.enable_container or not module.enabled:
-      return
-
-    if not self._is_module_enabled(module, device):
-      return
-
     LOGGER.info("Running test module " + module.name)
 
     try:
@@ -339,14 +347,13 @@ class TestOrchestrator:
         module_results = module_results_json["results"]
         for test_result in module_results:
           self._session.add_test_result(test_result)
-          self._session.add_total_tests(1)
     except (FileNotFoundError, PermissionError,
             json.JSONDecodeError) as results_error:
       LOGGER.error(
           f"Error occured whilst obbtaining results for module {module.name}")
       LOGGER.debug(results_error)
 
-    LOGGER.info("Test module " + module.name + " has finished")
+    LOGGER.info(f"Test module {module.name} has finished")
 
   def _get_module_status(self, module):
     container = self._get_module_container(module)
