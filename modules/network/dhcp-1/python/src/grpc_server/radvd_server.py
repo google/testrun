365fd23486fd837a252e9df2ed18d64155565a72
jhughesbiot <jonathan.hughes@buildingsiot.com>
2023-10-09 08:59:09 -0600

    Change radvd config to better control broadcast rate and advertise lifetime Add a disable feature to the radvd server and disable in dhcp-2 image Misc updates to ipv6 and slaac test methods and logging

--- modules/network/dhcp-1/python/src/grpc_server/radvd_server.py
+++ modules/network/dhcp-1/python/src/grpc_server/radvd_server.py
@@ -22,29 +22,42 @@ LOGGER = None
 class RADVDServer:
   """Represents the RADVD Server"""
 
-  def __init__(self):
+  def __init__(self, enabled=True):
     global LOGGER
-    LOGGER = logger.get_logger(LOG_NAME, 'dhcp-1')
+    LOGGER = logger.get_logger(LOG_NAME, 'dhcp-2')
+    self.enabled = enabled
 
   def restart(self):
+    if not self.enabled:
+      LOGGER.info('Cannot restart RADVD server, disabled')
+      return False
     LOGGER.info('Restarting RADVD server')
     response = util.run_command('radvd-service restart', False)
     LOGGER.info('RADVD restarted: ' + str(response))
     return response
 
   def start(self):
+    if not self.enabled:
+      LOGGER.info('Cannot start RADVD server, disabled')
+      return False
     LOGGER.info('Starting RADVD server')
     response = util.run_command('radvd-service start', False)
     LOGGER.info('RADVD started: ' + str(response))
     return response
 
   def stop(self):
+    if not self.enabled:
+      LOGGER.info('Cannot stop RADVD server, disabled')
+      return False
     LOGGER.info('Stopping RADVD server')
     response = util.run_command('radvd-service stop', False)
     LOGGER.info('RADVD stopped: ' + str(response))
     return response
 
   def is_running(self):
+    if not self.enabled:
+      LOGGER.info('RADVD server disabled')
+      return False
     LOGGER.info('Checking RADVD status')
     response = util.run_command('radvd-service status')
     running = response[0] == 'radvd service is running.'
