b810113e848b3708c77f124da8a68832ead4af31
J Boddey <boddey@google.com>
2024-02-21 13:28:15 +0000

    Add steps to resolve to dev (#257)
    * Add initial steps to resolve content
    
    * Load steps to resolve into test cases
    
    * Rename steps to resolve to recommendations

--- framework/python/src/test_orc/test_orchestrator.py
+++ framework/python/src/test_orc/test_orchestrator.py
@@ -404,6 +404,7 @@ class TestOrchestrator:
         self._root_path,
         "runtime/test/" + device.mac_addr.replace(":", "") + "/" + module.name)
     results_file = f"{container_runtime_dir}/{module.name}-result.json"
+
     try:
       with open(results_file, "r", encoding="utf-8-sig") as f:
         module_results_json = json.load(f)
@@ -416,6 +417,7 @@ class TestOrchestrator:
         f"Error occurred whilst obtaining results for module {module.name}")
 
       LOGGER.error(results_error)
+
     # Get report from the module
     report_file = f"{container_runtime_dir}/{module.name}_report.md"
     try:
@@ -507,6 +509,10 @@ class TestOrchestrator:
             expected_behavior=test_case_json["expected_behavior"],
             required_result=test_case_json["required_result"]
           )
+
+          if "recommendations" in test_case_json:
+            test_case.recommendations = test_case_json["recommendations"]
+
           module.tests.append(test_case)
         except Exception as error:
           LOGGER.error("Failed to load test case. See error for details")
