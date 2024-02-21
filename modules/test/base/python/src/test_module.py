f95ef67f97676a0cef6693113a55ee5b841b3171
J Boddey <boddey@google.com>
2024-02-21 13:28:15 +0000

    Add steps to resolve to dev (#257)
    * Add initial steps to resolve content
    
    * Load steps to resolve into test cases
    
    * Rename steps to resolve to recommendations

--- modules/test/base/python/src/test_module.py
+++ modules/test/base/python/src/test_module.py
@@ -124,6 +124,11 @@ class TestModule:
         test['result'] = 'Skipped'
         test['description'] = 'An error occured whilst running this test'
 
+      # Remove the steps to resolve if compliant already
+      if (test['result'] == 'Compliant' and
+          'recommendations' in test):
+        test.pop('recommendations')
+
       test['end'] = datetime.now().isoformat()
       duration = datetime.fromisoformat(test['end']) - datetime.fromisoformat(
           test['start'])
