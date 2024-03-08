299fd12cf2eaa6ea93fd77a15ed7589ac54c92e5
Jacob Boddey <boddey@google.com>
2024-03-08 12:49:01 +0000

    Update check

--- modules/test/conn/python/src/connection_module.py
+++ modules/test/conn/python/src/connection_module.py
@@ -96,8 +96,10 @@ class ConnectionModule(TestModule):
       no_arp = False
 
       # Check MAC address matches IP address
-      if (arp_packet.hwsrc != self._device_mac or
+      if (arp_packet.hwsrc == self._device_mac and
           arp_packet.psrc != self._device_ipv4_addr):
+        LOGGER.info(f'Bad ARP packet detected for MAC: {self._device_mac}')
+        LOGGER.info(f'ARP packet IP {arp_packet.psrc} does not match {self._device_ipv4_addr}')
         return False, 'Device is sending false ARP response'
 
     if no_arp:
