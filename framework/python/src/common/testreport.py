ad4ad61d8768c6522b9f8da3e9e66740fd6078f1
J Boddey <boddey@google.com>
2024-04-15 20:34:24 +0100

    Fix modbus test and bump version (#394)

--- framework/python/src/common/testreport.py
+++ framework/python/src/common/testreport.py
@@ -57,7 +57,7 @@ class TestReport():
     self._report_url = ''
     self._cur_page = 0
     # Placeholder until available in json report
-    self._version = 'v1.2.1-alpha'
+    self._version = 'v1.2.2-alpha'
 
   def add_module_reports(self, module_reports):
     self._module_reports = module_reports
