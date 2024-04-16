c5e9540466a2422b8d8a5d9ae64d4052c9385898
Jacob Boddey <boddey@google.com>
2024-04-16 09:22:57 +0100

    Fix pylint issue

--- framework/python/src/net_orc/network_orchestrator.py
+++ framework/python/src/net_orc/network_orchestrator.py
@@ -258,7 +258,8 @@ class NetworkOrchestrator:
         sniffer.stop()
         self._session.set_status('Cancelled')
         LOGGER.error('Device interface disconnected, cancelling Testrun')
-    wrpcap(os.path.join(device_runtime_dir, 'monitor.pcap'), self._monitor_packets)
+    wrpcap(os.path.join(device_runtime_dir, 'monitor.pcap'),
+           self._monitor_packets)
     self._monitor_in_progress = False
     self.get_listener().call_callback(NetworkEvent.DEVICE_STABLE,
                                       device.mac_addr)
