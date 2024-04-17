58dec567ad0bad3f21fecaf2697780c0bf8de1e9
jhughesbiot <50999916+jhughesbiot@users.noreply.github.com>
2024-04-17 05:31:35 -0600

    Expand pylint to whole project and fix simple pylint issues (#401)

--- testing/unit/tls/tls_module_test.py
+++ testing/unit/tls/tls_module_test.py
@@ -27,19 +27,20 @@ import http.client
 MODULE = 'tls'
 # Define the file paths
 TEST_FILES_DIR = 'testing/unit/' + MODULE
-OUTPUT_DIR = os.path.join(TEST_FILES_DIR,'output/')
-REPORTS_DIR = os.path.join(TEST_FILES_DIR,'reports/')
-CAPTURES_DIR = os.path.join(TEST_FILES_DIR,'captures/')
-CERT_DIR = os.path.join(TEST_FILES_DIR,'certs/')
-ROOT_CERTS_DIR = os.path.join(TEST_FILES_DIR,'root_certs')
+OUTPUT_DIR = os.path.join(TEST_FILES_DIR, 'output/')
+REPORTS_DIR = os.path.join(TEST_FILES_DIR, 'reports/')
+CAPTURES_DIR = os.path.join(TEST_FILES_DIR, 'captures/')
+CERT_DIR = os.path.join(TEST_FILES_DIR, 'certs/')
+ROOT_CERTS_DIR = os.path.join(TEST_FILES_DIR, 'root_certs')
 
-LOCAL_REPORT = os.path.join(REPORTS_DIR,'tls_report_local.md')
-LOCAL_REPORT_EXT = os.path.join(REPORTS_DIR,'tls_report_ext_local.md')
-LOCAL_REPORT_NO_CERT = os.path.join(REPORTS_DIR,'tls_report_no_cert_local.md')
+LOCAL_REPORT = os.path.join(REPORTS_DIR, 'tls_report_local.md')
+LOCAL_REPORT_EXT = os.path.join(REPORTS_DIR, 'tls_report_ext_local.md')
+LOCAL_REPORT_NO_CERT = os.path.join(REPORTS_DIR, 'tls_report_no_cert_local.md')
 CONF_FILE = 'modules/test/' + MODULE + '/conf/module_config.json'
 
 TLS_UTIL = None
 PACKET_CAPTURE = None
+
 
 class TLSModuleTest(unittest.TestCase):
   """Contains and runs all the unit tests concerning TLS behaviors"""
@@ -148,7 +149,7 @@ class TLSModuleTest(unittest.TestCase):
     tls_1_2_results = True, success_message
     tls_1_3_results = True, success_message
     expected = True, (f'TLS 1.2 validated: {success_message}\n'
-      f'TLS 1.3 validated: {success_message}')
+                      f'TLS 1.3 validated: {success_message}')
     result = TLS_UTIL.process_tls_server_results(tls_1_2_results,
                                                  tls_1_3_results)
     self.assertEqual(result, expected)
@@ -171,11 +172,10 @@ class TLSModuleTest(unittest.TestCase):
                                                  tls_1_3_results)
     self.assertEqual(result, expected)
 
-
     # TLS 1.2 Fail and TLS 1.2 Fail
     tls_1_3_results = False, fail_message
     expected = False, (f'TLS 1.2 not validated: {fail_message}\n'
-                      f'TLS 1.3 not validated: {fail_message}')
+                       f'TLS 1.3 not validated: {fail_message}')
     result = TLS_UTIL.process_tls_server_results(tls_1_2_results,
                                                  tls_1_3_results)
     self.assertEqual(result, expected)
@@ -301,7 +301,7 @@ class TLSModuleTest(unittest.TestCase):
   def tls_module_report_test(self):
     print('\ntls_module_report_test')
     os.environ['DEVICE_MAC'] = '38:d1:35:01:17:fe'
-    pcap_file = os.path.join(CAPTURES_DIR,'tls.pcap')
+    pcap_file = os.path.join(CAPTURES_DIR, 'tls.pcap')
     tls = TLSModule(module=MODULE,
                     log_dir=OUTPUT_DIR,
                     conf_file=CONF_FILE,
@@ -323,7 +323,7 @@ class TLSModuleTest(unittest.TestCase):
   def tls_module_report_ext_test(self):
     print('\ntls_module_report_ext_test')
     os.environ['DEVICE_MAC'] = '28:29:86:27:d6:05'
-    pcap_file = os.path.join(CAPTURES_DIR,'tls_ext.pcap')
+    pcap_file = os.path.join(CAPTURES_DIR, 'tls_ext.pcap')
     tls = TLSModule(module=MODULE,
                     log_dir=OUTPUT_DIR,
                     conf_file=CONF_FILE,
@@ -336,7 +336,7 @@ class TLSModuleTest(unittest.TestCase):
   def tls_module_report_no_cert_test(self):
     print('\ntls_module_report_no_cert_test')
     os.environ['DEVICE_MAC'] = ''
-    pcap_file = os.path.join(CAPTURES_DIR,'tls_ext.pcap')
+    pcap_file = os.path.join(CAPTURES_DIR, 'tls_ext.pcap')
     tls = TLSModule(module=MODULE,
                     log_dir=OUTPUT_DIR,
                     conf_file=CONF_FILE,
@@ -443,15 +443,17 @@ class TLSModuleTest(unittest.TestCase):
 
   def tls_module_trusted_ca_cert_chain_test(self):
     print('\ntls_module_trusted_ca_cert_chain_test')
-    cert_path = os.path.join(CERT_DIR,'_.google.com.crt')
+    cert_path = os.path.join(CERT_DIR, '_.google.com.crt')
     cert_valid = TLS_UTIL.validate_cert_chain(device_cert_path=cert_path)
     self.assertEqual(cert_valid, True)
 
   def tls_module_local_ca_cert_test(self):
     print('\ntls_module_trusted_ca_cert_chain_test')
-    cert_path = os.path.join(CERT_DIR,'device_cert_local.crt')
-    cert_valid = TLS_UTIL.validate_local_ca_signature(device_cert_path=cert_path)
+    cert_path = os.path.join(CERT_DIR, 'device_cert_local.crt')
+    cert_valid = TLS_UTIL.validate_local_ca_signature(
+        device_cert_path=cert_path)
     self.assertEqual(cert_valid[0], True)
+
 
 if __name__ == '__main__':
   suite = unittest.TestSuite()
