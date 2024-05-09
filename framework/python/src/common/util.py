3672a347977293cf2c1c03a840b1b2ad7e98935c
Jacob Boddey <boddey@google.com>
2024-05-09 09:28:00 +0100

    Fix PDF alignment

--- framework/python/src/common/util.py
+++ framework/python/src/common/util.py
@@ -105,7 +105,7 @@ def get_module_display_name(search):
     'ntp': 'NTP',
     'dns': 'DNS',
     'connection': 'Connection',
-    'nmap': 'Services',
+    'services': 'Services',
     'tls': 'TLS',
     'protocol': 'Protocol'
   }
