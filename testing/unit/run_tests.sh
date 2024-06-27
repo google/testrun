f2a5ccfc5b540318f7cb0082aef6eabd39a4de7f
jhughesbiot <jonathan.hughes@buildingsiot.com>
2024-06-27 03:54:36 -0600

    Feature/risk profile (#522)
    * Update risk profile
    
    * Add unit tests for risk profile
    Fix service nmap unit test
    
    * Add unit tests for risk profile
    Fix service nmap unit test
    
    * Update api and session for new risk profile methods
    
    * pylint
    
    * pylint
    
    * Fix some pylint
    
    * Fix more pylint
    
    * Fix some bugs
    
    * Update risk profile logic (#535)
    
    * Update risk profile logic
    
    * Update test profiles
    Remove duplicate test file
    Cleanup temp test files
    
    * Remove categories from profile
    
    * Update expiration check to account for leap years
    
    * Update risk assessment questions
    
    * Update dependency
    
    * Add missing question
    
    * Update format and add error handling
    
    ---------
    
    Co-authored-by: jhughesbiot <jonathan.hughes@buildingsiot.com>
    
    ---------
    
    Signed-off-by: J Boddey <boddey@google.com>
    Co-authored-by: Jacob Boddey <boddey@google.com>

--- testing/unit/run_tests.sh
+++ testing/unit/run_tests.sh
@@ -30,32 +30,35 @@ PYTHONPATH="$PYTHONPATH:$PWD/modules/test/base/python/src"
 PYTHONPATH="$PYTHONPATH:$PWD/modules/test/conn/python/src"
 PYTHONPATH="$PYTHONPATH:$PWD/modules/test/tls/python/src"
 PYTHONPATH="$PYTHONPATH:$PWD/modules/test/dns/python/src"
-PYTHONPATH="$PYTHONPATH:$PWD/modules/test/nmap/python/src"
+PYTHONPATH="$PYTHONPATH:$PWD/modules/test/services/python/src"
 PYTHONPATH="$PYTHONPATH:$PWD/modules/test/ntp/python/src"
 
 # Set the python path with all sources
 export PYTHONPATH
 
-# # Run the DHCP Unit tests
-# python3 -u $PWD/modules/network/dhcp-1/python/src/grpc_server/dhcp_config_test.py
-# python3 -u $PWD/modules/network/dhcp-2/python/src/grpc_server/dhcp_config_test.py
+# Run the DHCP Unit tests
+python3 -u $PWD/modules/network/dhcp-1/python/src/grpc_server/dhcp_config_test.py
+python3 -u $PWD/modules/network/dhcp-2/python/src/grpc_server/dhcp_config_test.py
 
-# # Run the Conn Module Unit Tests
-# python3 -u $PWD/testing/unit/conn/conn_module_test.py
+# Run the Conn Module Unit Tests
+python3 -u $PWD/testing/unit/conn/conn_module_test.py
 
 # Run the TLS Module Unit Tests
 python3 -u $PWD/testing/unit/tls/tls_module_test.py
 
-# # Run the DNS Module Unit Tests
-# python3 -u $PWD/testing/unit/dns/dns_module_test.py
+# Run the DNS Module Unit Tests
+python3 -u $PWD/testing/unit/dns/dns_module_test.py
 
-# # Run the NMAP Module Unit Tests
-# python3 -u $PWD/testing/unit/nmap/nmap_module_test.py
+# Run the NMAP Module Unit Tests
+python3 -u $PWD/testing/unit/services/services_module_test.py
 
-# # Run the NTP Module Unit Tests
-# python3 -u $PWD/testing/unit/ntp/ntp_module_test.py
+# Run the NTP Module Unit Tests
+python3 -u $PWD/testing/unit/ntp/ntp_module_test.py
 
-# # # Run the Report Unit Tests
-# python3 -u $PWD/testing/unit/report/report_test.py
+# Run the Report Unit Tests
+python3 -u $PWD/testing/unit/report/report_test.py
+
+# Run the RiskProfile Unit Tests
+python3 -u $PWD/testing/unit/risk_profile/risk_profile_test.py
 
 popd >/dev/null 2>&1
