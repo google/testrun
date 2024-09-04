f6478bb33107ce3f89c1f1b62431bde3cce8ea8e
Sofia Kurilova <kurilova@google.com>
2024-09-04 14:29:22 +0200

    Adds dialog if profile is successfully saved (#746)
    * Adds dialog if profile is successfully saved

---
+++ modules/ui/src/app/pages/risk-assessment/components/success-dialog/success-dialog.component.spec.ts
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SuccessDialogComponent } from './success-dialog.component';
+import { TestRunService } from '../../../../services/test-run.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { PROFILE_MOCK } from '../../../../mocks/profile.mock';
+
+describe('SuccessDialogComponent', () => {
+  let component: SuccessDialogComponent;
+  let fixture: ComponentFixture<SuccessDialogComponent>;
+  const testRunServiceMock = jasmine.createSpyObj(['getRiskClass']);
+  let compiled: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SuccessDialogComponent],
+      providers: [
+        { provide: TestRunService, useValue: testRunServiceMock },
+        {
+          provide: MatDialogRef,
+          useValue: {
+            keydownEvents: () => of(new KeyboardEvent('keydown', { code: '' })),
+            close: () => ({}),
+          },
+        },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
+    }).compileComponents();
+    fixture = TestBed.createComponent(SuccessDialogComponent);
+    component = fixture.componentInstance;
+    component.data = {
+      profile: PROFILE_MOCK,
+    };
+    compiled = fixture.nativeElement as HTMLElement;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should close dialog on "cancel" click', () => {
+    const closeSpy = spyOn(component.dialogRef, 'close');
+    const confirmButton = compiled.querySelector(
+      '.confirm-button'
+    ) as HTMLButtonElement;
+
+    confirmButton?.click();
+
+    expect(closeSpy).toHaveBeenCalled();
+
+    closeSpy.calls.reset();
+  });
+});
