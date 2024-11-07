7e9311ff1c846d4eeba065579f2475041cd84cf7
Jacob Boddey <boddey@google.com>
2024-11-07 09:41:22 +0000

    Add BACnet test and extend port scan

--- modules/test/services/python/src/services_module.py
+++ modules/test/services/python/src/services_module.py
@@ -196,10 +196,9 @@ class ServicesModule(TestModule):
       self._scan_results.update(self._scan_udp_results)
 
   def _scan_tcp_ports(self):
-    max_port = 10000
     LOGGER.info('Running nmap TCP port scan')
     nmap_results = util.run_command( # pylint: disable=E1120
-        f'''nmap --open -sT -sV -Pn -v -p 1-{max_port}
+        f'''nmap --open -sT -sV -Pn -v -p 1-65535
       --version-intensity 7 -T4 -oX - {self._ipv4_addr}''')[0]
 
     LOGGER.info('TCP port scan complete')
