e74b33fa135d8528117b4aa426aebfd001fbe1ec
Jacob Boddey <boddey@google.com>
2024-11-22 12:04:24 +0000

    Add additional device config info and generate pdf

--- testing/unit/report/report_test.py
+++ testing/unit/report/report_test.py
@@ -71,11 +71,15 @@ class ReportTest(unittest.TestCase):
 
     # Create the HTML filename based on the JSON name
     file_name = os.path.splitext(os.path.basename(results_file_path))[0]
-    report_out_file = os.path.join(OUTPUT_DIR, file_name + '.html')
+    report_html_file = os.path.join(OUTPUT_DIR, file_name + '.html')
+    report_pdf_file = os.path.join(OUTPUT_DIR, file_name + '.pdf')
 
      # Save report as HTML file
-    with open(report_out_file, 'w', encoding='utf-8') as file:
+    with open(report_html_file, 'w', encoding='utf-8') as file:
       file.write(report.to_html())
+
+    with open(report_pdf_file, 'w', encoding='utf-8') as file:
+      file.write(report.to_pdf())
 
   def report_compliant_test(self):
     """Generate a report for the compliant test"""
