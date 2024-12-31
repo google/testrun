410b8f6f34a75a2457b1d79877d3e438767e391a
Volha Mardvilka <mardvilka@google.com>
2024-12-31 11:09:33 +0000

    385715473: (feat) add TLS callout to certificates tab

--- modules/ui/src/app/pages/certificates/components/certificates-table/certificates-table.component.ts
+++ modules/ui/src/app/pages/certificates/components/certificates-table/certificates-table.component.ts
@@ -4,6 +4,8 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { CalloutType } from '../../../../model/callout-type';
+import { CalloutComponent } from '../../../../components/callout/callout.component';
 import { EmptyMessageComponent } from '../../../../components/empty-message/empty-message.component';
 
 @Component({
@@ -13,6 +15,7 @@ import { EmptyMessageComponent } from '../../../../components/empty-message/empt
     MatTableModule,
     MatButtonModule,
     CommonModule,
+    CalloutComponent,
     EmptyMessageComponent,
   ],
   templateUrl: './certificates-table.component.html',
@@ -20,6 +23,7 @@ import { EmptyMessageComponent } from '../../../../components/empty-message/empt
 })
 export class CertificatesTableComponent {
   dataSource = input.required<MatTableDataSource<Certificate>>();
+  readonly CalloutType = CalloutType;
   selectedCertificate = input<string>();
   dataLoaded = input<boolean>(false);
   displayedColumns = input<string[]>([]);
