72f092ec561d33927be231f44f75657ff6955b3b
Jacob Boddey <boddey@google.com>
2025-03-25 15:41:34 +0000

    Fix pylint issue

--- modules/test/dns/python/src/dns_module.py
+++ modules/test/dns/python/src/dns_module.py
@@ -269,8 +269,10 @@ class DNSModule(TestModule):
         LOGGER.info('DNS traffic detected only to configured DHCP DNS server')
         description = 'DNS traffic detected only to DHCP provided server'
     else:
-      LOGGER.info('No DNS traffic detected from the device to the DHCP DNS server')
-      description = 'No DNS traffic detected from the device to the DHCP DNS server'
+      LOGGER.info(
+        'No DNS traffic detected from the device to the DHCP DNS server')
+      description = '' \
+      'No DNS traffic detected from the device to the DHCP DNS server'
     return 'Informational', description
 
   def _dns_network_hostname_resolution(self):
