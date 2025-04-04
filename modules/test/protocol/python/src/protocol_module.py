2e24937866b4c8555876a81a7e63136b92eb82ae
jhughesbiot <jonathan.hughes@buildingsiot.com>
2025-04-04 02:48:55 -0600

    Resolve and use device address for reads (#1215)

--- modules/test/protocol/python/src/protocol_module.py
+++ modules/test/protocol/python/src/protocol_module.py
@@ -71,9 +71,10 @@ class ProtocolModule(TestModule):
     if len(self._bacnet.devices) > 0:
       for device in self._bacnet.devices:
         LOGGER.debug(f'Checking BACnet version for device: {device}')
+        device_addr = device[2]
+        device_id = device[3]
         result_status, result_description = \
-          self._bacnet.validate_protocol_version(self._device_ipv4_addr,
-            device[3])
+          self._bacnet.validate_protocol_version(device_addr,device_id)
         break
 
     LOGGER.info(result_description)
