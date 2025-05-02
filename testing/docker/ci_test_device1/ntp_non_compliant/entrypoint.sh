6bc1a6831d03dd6d1f35cb37def61e86ac41e552
MariusBaldovin <baldovin@google.com>
2025-05-02 13:56:11 +0100

    add ntp compliant and non compliant tests

---
+++ testing/docker/ci_test_device1/ntp_non_compliant/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+# Display network interfaces
+ip a
+
+# Set paths and servers
+NTP_SERVER=10.10.10.5
+INTF=eth0
+
+# DHCP
+ip addr flush dev $INTF
+PID_FILE=/var/run/dhclient.pid
+if [ -f $PID_FILE ]; then
+    kill -9 $(cat $PID_FILE) || true
+    rm -f $PID_FILE
+fi
+dhclient -v $INTF
+DHCP_TPID=$!
+echo $DHCP_TPID
+
+# Keep network monitoring (can refactor later for other network modules)
+(while true; do arping 10.10.10.1; sleep 10; done) &
+(while true; do ip a | cat; sleep 10; done) &
+
+# Keep the script running
+tail -f /dev/null
+
