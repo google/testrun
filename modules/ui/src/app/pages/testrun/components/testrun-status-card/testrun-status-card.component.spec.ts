7e44c71f7789430be49ae50063f363be01fdf579
kurilova <kurilova@google.com>
2025-10-17 11:25:23 +0000

    Change the method to count test results

--- modules/ui/src/app/pages/testrun/components/testrun-status-card/testrun-status-card.component.spec.ts
+++ modules/ui/src/app/pages/testrun/components/testrun-status-card/testrun-status-card.component.spec.ts
@@ -152,7 +152,7 @@ describe('ProgressStatusCardComponent', () => {
       });
 
       it('should return correct test result if status "Compliant"', () => {
-        const expectedResult = '2/3';
+        const expectedResult = '2/26';
 
         const result = component.getTestsResult(MOCK_PROGRESS_DATA_COMPLIANT);
 
