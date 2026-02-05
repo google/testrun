b48c2578fafb54f9f1411a74e63fbb8159ecb318
kurilova <kurilova@google.com>
2026-02-05 14:27:28 +0000

    Revert "Adds logo"
    This reverts commit 2255ccbda33ce0b2b761c7766984e9d2eafa3dda.

--- modules/test/base/python/src/test_module.py
+++ modules/test/base/python/src/test_module.py
@@ -268,8 +268,7 @@ class TestModule:
 
     report_jinja_styled = template.render(
         template=module_template,
-        styles=styles,
-        logo=logo,
+        styles=styles
     )
     # Write the styled content to a file
     with open(result_path, 'w', encoding='utf-8') as file:
