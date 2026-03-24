e88c57b40b5b39825a44df4312d425cfa6015a8f
Aliaksandr Nikitsin <aliaksandrn@google.com>
2026-03-24 20:09:47 +0100

    remove port from get_ciphers

--- modules/test/tls/bin/get_ciphers.sh
+++ modules/test/tls/bin/get_ciphers.sh
@@ -18,7 +18,7 @@ CAPTURE_FILE="$1"
 DST_IP="$2"
 DST_PORT="$3"
 
-TSHARK_FILTER="ssl.handshake.ciphersuites and ip.dst==$DST_IP and tcp.dstport==$DST_PORT"
+TSHARK_FILTER="ssl.handshake.ciphersuites and ip.dst==$DST_IP"
 response=$(tshark -r "$CAPTURE_FILE" -Y "$TSHARK_FILTER" -Vx | grep 'Cipher Suite:' | awk '{$1=$1};1' | sed 's/Cipher Suite: //')
 
 echo "$response"
