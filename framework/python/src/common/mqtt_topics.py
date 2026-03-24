b4d6bb960f71cbff64bb432dbd8581127f4128e5
kurilova <kurilova@google.com>
2026-03-24 14:37:49 +0000

    Fix pylint

--- framework/python/src/common/mqtt_topics.py
+++ framework/python/src/common/mqtt_topics.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Enums for mqtt topics"""
 from enum import StrEnum
 
 class MQTTTopic(StrEnum):
   INFO = "info"
-  INTERNET_CONNECTION_TOPIC = 'events/internet'
-  NETWORK_ADAPTERS_TOPIC = 'events/adapter'
-  STATUS_TOPIC = 'status'
+  INTERNET_CONNECTION_TOPIC = "events/internet"
+  NETWORK_ADAPTERS_TOPIC = "events/adapter"
+  STATUS_TOPIC = "status"
