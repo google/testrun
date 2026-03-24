9c6429180b6c04bf5da55cb45f00feef45fba859
kurilova <kurilova@google.com>
2026-03-24 14:30:02 +0000

    Adds lazy loading

--- framework/python/src/common/logger.py
+++ framework/python/src/common/logger.py
@@ -16,12 +16,12 @@
 import json
 import logging
 import os
-from common.mqtt import MQTT
 from common.mqtt_topics import MQTTTopic
 
 
 class TestrunLogger(logging.Logger):
   def ui_info(self, msg, *args, **kwargs):
+    from common.mqtt import MQTT
     with MQTT() as client:
       client.send_message(MQTTTopic.INFO, {'message': msg})
     self.info(msg, *args, **kwargs)
