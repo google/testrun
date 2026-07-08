8bf1639174e429d781c1a04e6003046a5e79bd8c
Aliaksandr Nikitsin <aliaksandrn@google.com>
2026-07-08 22:20:43 +0200

    remove unused argument

--- modules/test/tls/bin/get_ciphers.sh
+++ modules/test/tls/bin/get_ciphers.sh
@@ -16,7 +16,6 @@
 
 CAPTURE_FILE="$1"
 DST_IP="$2"
-DST_PORT="$3"
 
 TSHARK_FILTER="ssl.handshake.ciphersuites and ip.dst==$DST_IP"
 response=$(tshark -r "$CAPTURE_FILE" -Y "$TSHARK_FILTER" -Vx | grep 'Cipher Suite:' | awk '{$1=$1};1' | sed 's/Cipher Suite: //')
