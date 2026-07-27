b2d2ca982c234ad49070954b2afd692821ced94f
Aliaksandr Nikitsin <aliaksandrn@google.com>
2026-07-27 21:18:31 +0200

    fix service_non_compliant bash script

--- testing/docker/ci_test_device1/bacnet_compliant/entrypoint.sh
+++ testing/docker/ci_test_device1/bacnet_compliant/entrypoint.sh
@@ -9,7 +9,7 @@ INTF=eth0
 ip addr flush dev $INTF
 PID_FILE=/var/run/dhclient.pid
 if [ -f $PID_FILE ]; then
-    kill -9 $(cat $PID_FILE) || true
+    kill -9 "$(cat $PID_FILE)" || true
     rm -f $PID_FILE
 fi
 dhclient -v $INTF
