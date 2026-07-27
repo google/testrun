24141f9935c04a71b80f20463b587bcbaf2160c4
Aliaksandr Nikitsin <aliaksandrn@google.com>
2026-07-27 19:36:52 +0200

    refactor entrypoint.sh

--- testing/docker/ci_baseline/entrypoint.sh
+++ testing/docker/ci_baseline/entrypoint.sh
@@ -22,9 +22,9 @@ DNS_SERVER=10.10.10.4
 function wout(){
     temp=${1//./\".\"}
     key=${temp:1}\"
-    echo $key
+    echo "$key"
     value=$2
-    jq "$key+=\"$value\"" $OUT | sponge $OUT
+    jq "$key+=\"$value\"" "$OUT" | sponge "$OUT"
 }
 
 
@@ -33,9 +33,9 @@ dig @8.8.8.8 +short www.google.com
 # DHCP
 ip addr flush dev eth0
 PID_FILE=/var/run/dhclient.pid
-if [ -f $PID_FILE ]; then
-    kill -9 $(cat $PID_FILE) || true
-    rm -f $PID_FILE
+if [ -f "$PID_FILE" ]; then
+    kill -9 "$(cat "$PID_FILE")" || true
+    rm -f "$PID_FILE"
 fi
 
 # Set MAC address to match the only_baseline 
@@ -46,32 +46,32 @@ ip link set dev eth0 address "$desired_mac"
 
 dhclient -v eth0
 
-echo "{}" > $OUT
+echo "{}" > "$OUT"
 
 # Gen network
 main_intf=$(ip route | grep '^default' | awk '{print $NF}')
 
-wout .network.main_intf $main_intf 
-wout .network.gateway $(ip route | head -n 1 | awk '{print $3}')
-wout .network.ipv4 $(ip a show $main_intf | grep "inet " | awk '{print $2}')
-wout .network.ipv6 $(ip a show $main_intf | grep inet6 | awk '{print $2}')
-wout .network.ethmac $(cat /sys/class/net/$main_intf/address)
+wout .network.main_intf "$main_intf" 
+wout .network.gateway "$(ip route | head -n 1 | awk '{print $3}')"
+wout .network.ipv4 "$(ip a show "$main_intf" | grep "inet " | awk '{print $2}')"
+wout .network.ipv6 "$(ip a show "$main_intf" | grep inet6 | awk '{print $2}')"
+wout .network.ethmac "$(cat /sys/class/net/"$main_intf"/address)"
 
-wout .dns_response $(dig @$DNS_SERVER +short www.google.com | tail -1)
-wout .ntp_offset $(ntpdate -q $NTP_SERVER | tail -1 | sed -E 's/.*offset ([-=0-9\.]*) sec/\1/')
+wout .dns_response "$(dig @"$DNS_SERVER" +short www.google.com | tail -1)"
+wout .ntp_offset "$(ntpdate -q "$NTP_SERVER" | tail -1 | sed -E 's/.*offset ([-=0-9\.]*) sec/\1/')"
 
 # INTERNET CONNECTION
 google_com_response=$(curl -LI http://www.google.com -o /dev/null -w '%{http_code}\n' -s)
-wout .network.internet $google_com_response
+wout .network.internet "$google_com_response"
 
 # DHCP LEASE
-while read pre name value; do
+while read -r pre name value; do
     if [[ $pre != option ]]; then
         continue;
     fi
 
-    wout .dhcp.$name $(echo "${value%;}" | tr -d '\"\\')
+    wout .dhcp."$name" "$(echo "${value%;}" | tr -d '\"\\')"
  
 done < <(grep -B 99 -m 1 "}" /var/lib/dhcp/dhclient.leases)
 
-cat $OUT
+cat "$OUT"
