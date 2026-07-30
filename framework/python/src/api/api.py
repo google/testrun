5618ca068be299a1d3a193e4a2b01d9dca14f90a
Aliaksandr Nikitsin <aliaksandrn@google.com>
2026-07-30 18:10:28 +0200

    move disable comment

--- framework/python/src/api/api.py
+++ framework/python/src/api/api.py
@@ -139,7 +139,6 @@ class Api:
                                methods=["POST"])
 
     # Allow all origins to access the API
-    # nosemgrep: python.fastapi.security.wildcard-cors.wildcard-cors
     origins = ["*"]
 
     # Scheduler for background periodic tasks
@@ -154,6 +153,7 @@ class Api:
     # Attach CORS middleware
     self._app.add_middleware(
         CORSMiddleware,
+        # nosemgrep: python.fastapi.security.wildcard-cors.wildcard-cors
         allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
