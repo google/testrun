da66cc8699b7fba5bc229130df7ed81c5683336a
Aliaksandr Nikitsin <aliaksandrn@google.com>
2026-07-31 15:22:24 +0200

    disable semgrep

--- test_vm/python/src/modbus_server_non_compliant.py
+++ test_vm/python/src/modbus_server_non_compliant.py
@@ -36,9 +36,9 @@ def handle_request(conn, data):
 
 
 def run_server():
+  #nosemgrep
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  #nosemgrep
   sock.bind(("0.0.0.0", MODBUS_PORT))
   sock.listen(5)
   print(f"Modbus server started on port {MODBUS_PORT}")
