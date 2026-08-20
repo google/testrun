8ad74a39d3253bc52c8ac703498ba82f08f7bcd7
Aliaksandr Nikitsin <aliaksandrn@google.com>
2026-08-20 20:20:00 +0200

    refactor testreport delimiter

--- framework/python/src/common/testreport.py
+++ framework/python/src/common/testreport.py
@@ -180,10 +180,12 @@ class TestReport():
     for test in self._results:
       details = test.details
       if isinstance(details, str):
-        details = ' '.join(list(filter(lambda s: s != '', details.split('\n'))))
+        details = '; '.join(
+          list(filter(lambda s: s != '', details.split('\n')))
+        )
       if isinstance(details, list):
         details = [str(d) for d in details]
-        details = ' '.join(details)
+        details = '; '.join(details)
       else:
         details = str(details)
       test_dict = {
