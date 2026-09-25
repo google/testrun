c7cad2b76fd44e84e529e4ce4eafa065c4d02891
Aliaksandr Nikitsin <aliaksandrn@google.com>
2026-09-25 18:35:20 +0200

    pylint

--- test_vm/python/src/http_server.py
+++ test_vm/python/src/http_server.py
@@ -1,11 +1,16 @@
+"""Minimal HTTP backend used by the non-compliant VM test."""
 import sys
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class NonCompliantBackendHandler(BaseHTTPRequestHandler):
 
-  def log_message(self, format, *args):
+class NonCompliantBackendHandler(BaseHTTPRequestHandler):
+  """Serve deliberately non-compliant HTTP responses for VM tests."""
+
+  def log_message(self, log_format, *args):
     pass
 
+  # These names are required by BaseHTTPRequestHandler's dispatch protocol.
+  # pylint: disable=invalid-name
   def do_GET(self):
     self.send_response(200)
     self.send_header('Content-Type', 'text/html; charset=utf-8')
@@ -19,6 +24,7 @@ class NonCompliantBackendHandler(BaseHTTPRequestHandler):
     self.send_header('Content-Length', '18')
     self.end_headers()
     self.wfile.write(b'Method Not Allowed')
+  # pylint: enable=invalid-name
 
   def dispatch_custom(self):
     self.send_response(200)
